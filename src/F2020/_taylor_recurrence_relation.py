0684df0e6bccfe0174b9a26c734d498919ddcb09
Raul Gomez <rgomez@tec.mx>
2025-03-19 23:28:19 -0600

    Reset

---
+++ src/F2020/_taylor_recurrence_relation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: Raul Gomez
+Date: 2025-03-19
+Description: 
+"""
+
+NUMBER_OF_INITIAL_COEFFICIENTS = 2
+NUMBER_OF_EXTRA_COEFFICIENTS = 1 
+
+def recurrence_relation(k,required_coefficient_list):
+    next_coefficient = (required_coefficient_list[0])/(k * (k-1))
+    return next_coefficient
+
