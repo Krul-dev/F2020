066d993c4d792a3555c361ee08d53f505ea9faac
Raul Gomez <rgomez@tec.mx>
2025-04-18 08:42:34 -0600

    Formatting fixes across the board

--- src/taylor_coefficients/_taylor_coefficients_formulas.py
+++ src/taylor_coefficients/_taylor_coefficients_formulas.py
@@ -5,19 +5,18 @@ Author: Raul Gomez
 Date: 2025-03-19
 Description: 
 """
-
-from numpy import exp, sin, cos, log, abs, sqrt 
+from numpy import exp, sin, cos, log, abs, sqrt
 from scipy.special import factorial
 
 
 def Analytic_Function(x):
-    y=sin(x)
-
+    y = sin(x)
     return y
 
+
 def Taylor_Coefficient_Function(k):
-    if k%2==0:
-        c_k = 0 
+    if k % 2 == 0:
+        c_k = 0
     else:
-        c_k = (-1)**((k-1)/2)/factorial(k)
+        c_k = ((-1)**((k - 1) / 2)) / factorial(k)
     return c_k
