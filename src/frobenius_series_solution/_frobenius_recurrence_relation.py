949b7756735c63b19147f6c129beb6957b1d9d28
Raul Gomez <rgomez@tec.mx>
2025-04-18 22:48:24 -0600

    Formatting fixes across the board

--- src/frobenius_series_solution/_frobenius_recurrence_relation.py
+++ src/frobenius_series_solution/_frobenius_recurrence_relation.py
@@ -8,9 +8,9 @@ Description:
 
 R_INDEX = 1
 NUMBER_OF_INITIAL_COEFFICIENTS = 1
-NUMBER_OF_EXTRA_COEFFICIENTS = 1 
+NUMBER_OF_EXTRA_COEFFICIENTS = 1
 
-def recurrence_relation(k,required_coefficient_list):
+
+def recurrence_relation(k, required_coefficient_list):
     next_coefficient = -(required_coefficient_list[0])/(k * (k + 2))
     return next_coefficient
-
--- src/frobenius_series_solution/frobenius_series_model.py
+++ src/frobenius_series_solution/frobenius_series_model.py
@@ -7,53 +7,55 @@ Description:
 """
 
 from frobenius_series_solution._frobenius_recurrence_relation import (
-        R_INDEX,
-        NUMBER_OF_EXTRA_COEFFICIENTS,
-        recurrence_relation 
-        )
+    R_INDEX,
+    NUMBER_OF_EXTRA_COEFFICIENTS,
+    recurrence_relation
+)
 
 from function_series import (
-        adjust_initial_coefficient_list,
-        generate_coefficient_list,
-        generate_function_series
-        )
+    adjust_initial_coefficient_list,
+    generate_coefficient_list,
+    generate_function_series
+)
 
-from function_series import generate_frobenius_function 
+from function_series import generate_frobenius_function
 
-# Define the term function 
+# Define the term function
 term_function = generate_frobenius_function(R_INDEX)
-        
 
 
 # Define the functions to compute the dependent variables
 
 
-
-# Function to generate the Frobenius approximation function 
+# Function to generate the Frobenius approximation function
 def generate_frobenius_approximation_function(initial_coefficient_list, number_of_required_coefficients):
-    # Adjust the initial coefficient list 
-    adjusted_initial_coefficient_list = adjust_initial_coefficient_list(initial_coefficient_list, NUMBER_OF_EXTRA_COEFFICIENTS)
-    extented_coefficient_list = generate_coefficient_list(recurrence_relation, adjusted_initial_coefficient_list, number_of_required_coefficients, NUMBER_OF_EXTRA_COEFFICIENTS) 
+    # Adjust the initial coefficient list
+    adjusted_initial_coefficient_list = adjust_initial_coefficient_list(
+        initial_coefficient_list, NUMBER_OF_EXTRA_COEFFICIENTS)
+    extented_coefficient_list = generate_coefficient_list(
+        recurrence_relation, adjusted_initial_coefficient_list, number_of_required_coefficients, NUMBER_OF_EXTRA_COEFFICIENTS)
     coefficient_list = extented_coefficient_list[NUMBER_OF_EXTRA_COEFFICIENTS:]
-    frobenius_approximation_function = generate_function_series(coefficient_list, term_function)
-    return frobenius_approximation_function 
+    frobenius_approximation_function = generate_function_series(
+        coefficient_list, term_function)
+    return frobenius_approximation_function
 
 
-# Define the FrobeniusSeriesModel class 
+# Define the FrobeniusSeriesModel class
 class FrobeniusSeriesModel:
     def __init__(self, xmin, xmax, initial_coefficient_list, number_of_required_coefficients):
        # Set the default values for the time range and the x-axis limits
-        self.xmin = xmin     
+        self.xmin = xmin
         self.xmax = xmax
         self.number_of_required_coefficients = number_of_required_coefficients
-        self.frobenius_approximation_function = generate_frobenius_approximation_function(initial_coefficient_list, number_of_required_coefficients)
+        self.frobenius_approximation_function = generate_frobenius_approximation_function(
+            initial_coefficient_list, number_of_required_coefficients)
 
-    # Define the methods to get the x-axis limits 
+    # Define the methods to get the x-axis limits
     def get_xmin(self):
-        return self.xmin 
+        return self.xmin
 
-    def get_xmax(self): 
-        return self.xmax 
+    def get_xmax(self):
+        return self.xmax
 
     def get_number_of_required_coefficients(self):
         return self.number_of_required_coefficients
