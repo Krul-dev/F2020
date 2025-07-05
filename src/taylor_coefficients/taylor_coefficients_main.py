3c4ec677a3d5c29956a8602ebdb5e2ec0f0c119b
Raul Gomez <rgomez@tec.mx>
2025-07-05 08:59:39 -0600

    Restoring project to linux machine

--- src/taylor_coefficients/taylor_coefficients_main.py
+++ src/taylor_coefficients/taylor_coefficients_main.py
@@ -10,6 +10,7 @@ from PyQt6.QtWidgets import QApplication
 from taylor_coefficients.taylor_coefficients_view import TaylorCoefficientsView
 from taylor_coefficients.taylor_coefficients_controller import TaylorCoefficientsController
 
+
 def main():
     app = QApplication(sys.argv)  # Initialize the application
 
@@ -17,13 +18,15 @@ def main():
     taylor_coefficients_view = TaylorCoefficientsView()
 
     # Create the controller and connect it to the view
-    taylor_coefficients_controller = TaylorCoefficientsController(taylor_coefficients_view)
+    taylor_coefficients_controller = TaylorCoefficientsController(
+        taylor_coefficients_view)
 
     # Show the view
     taylor_coefficients_view.show()
 
     # Start the application's event loop
     sys.exit(app.exec())
+
 
 if __name__ == "__main__":
     main()
